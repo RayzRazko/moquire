066ebbce29e711e1a10a574078361b36b5daa876
rayz <poliakov.rs@gmail.com>
2017-06-18 23:18:47 +0300

    Added unit tests for RestoreModule class

--- test/RestoreModule.test.js
+++ test/RestoreModule.test.js
@@ -1,1 +1,44 @@
 'use strict';
+const {expect} = require('chai');
+const RestoreModule = require('./../src/RestoreModule');
+
+describe('RestoreModule', function () {
+
+   describe('getPreviousModule', function () {
+
+       it('should return module passed to constructor', function () {
+           const restorer = new RestoreModule(__filename, module);
+           expect(restorer.getPreviousModule()).to.equal(module);
+       });
+
+   });
+
+
+   describe('restore', function () {
+
+       let testModule;
+       const testModulePath = require.resolve('./__testModule');
+
+       beforeEach(function () {
+           delete require.cache[testModulePath];
+           require('./__testModule');
+           testModule = require.cache[testModulePath];
+       });
+
+       it('should replace module in cache by previous module', function () {
+          const restorer = new RestoreModule(testModulePath, testModule);
+          require.cache[testModulePath] = {};
+          restorer.restore();
+          expect(require.cache[testModulePath]).to.equal(testModule);
+       });
+
+       it('should clear cache from module if previous state wasn\'t passed', function () {
+           delete require.cache[testModulePath];
+           const restorer = new RestoreModule(testModulePath);
+           require('./__testModule');
+           expect(require.cache).to.hasOwnProperty(testModulePath);
+           restorer.restore();
+           expect(require.cache).to.not.hasOwnProperty(testModulePath);
+       });
+   });
+});
